3f145fbda732c4480e1a757f5712d0d26342e1e1
Timofey Kuzmin <sinnerpocht@gmail.com>
2020-03-25 20:15:01 +0300

    Add new route companies with pagination example

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
-
+//ini_set('display_errors', 1);
+//error_reporting(E_ALL);
 // Подключение автозагрузки через composer
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -8,7 +9,24 @@ use Slim\Factory\AppFactory;
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
+
 $app->get('/', function ($request, $response) {
     return $response->write('Welcome to Slim!');
 });
+
+$companies = App\Generator::generate(100);
+
+
+/**
+ * Example page with pagination
+ */
+$app->get('/companies', function ($request, $response) use ($companies) {
+    $page = $request->getQueryParam('page', 1);
+    $per = $request->getQueryParam('per', 5);
+    $offset = ($page - 1) * $per;
+
+    $paginated_companies = array_slice($companies, $offset, $per);
+    return $response->write(json_encode($paginated_companies));
+});
+
 $app->run();
---
+++ tests/CompaniesRouteTest.php
@@ -0,0 +1,68 @@
+<?php
+
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+
+class CompaniesRouteTest extends TestCase
+{
+
+    private $client;
+
+    public function setUp(): void
+    {
+        $this->client = new \GuzzleHttp\Client([
+            'base_uri' => 'http://localhost:8080'
+        ]);
+    }
+
+    public function testCompanies1()
+    {
+        $expected = [
+            [ 'name' => 'Adams-Reichel','phone' => '1-986-987-9109 x56053' ],
+            [ 'name' => 'Dibbert-Morissette','phone' => '439.584.3132 x735' ],
+            [ 'name' => 'Ledner and Sons','phone' => '979-539-4173 x048' ],
+            [ 'name' => 'Kiehn-Mann','phone' => '972-379-1995 x61054' ],
+            [ 'name' => 'Bosco, Pouros and Larson','phone' => '887-919-2730 x49977' ]
+        ];
+        $response = $this->client->get('/companies');
+        $body = $response->getBody()->getContents();
+        $companies = json_decode($body);
+        $filteredCompanies = collect($companies)->map(function ($company) {
+            return ['name' => $company->name, 'phone' => $company->phone];
+        })->all();
+        $this->assertEquals($expected, $filteredCompanies);
+    }
+
+    public function testCompanies2()
+    {
+        $expected = [
+            [ 'name' => 'Ledner and Sons','phone' => '979-539-4173 x048' ],
+        ];
+        $response = $this->client->get('/companies?page=3&per=1');
+        $body = $response->getBody()->getContents();
+        $companies = json_decode($body);
+        $filteredCompanies = collect($companies)->map(function ($company) {
+            return ['name' => $company->name, 'phone' => $company->phone];
+        })->all();
+        $this->assertEquals($expected, $filteredCompanies);
+    }
+
+    public function testCompanies3()
+    {
+        $expected = [
+            [ 'name' => 'Zemlak-Wuckert', 'phone' => '291-495-8263 x678' ],
+            [ 'name' => 'Osinski, Kutch and Christiansen', 'phone' => '(843) 796-4156 x65355' ],
+        ];
+        $response = $this->client->get('/companies?page=20&per=2');
+        $body = $response->getBody()->getContents();
+        $companies = json_decode($body);
+        $filteredCompanies = collect($companies)->map(function ($company) {
+            return ['name' => $company->name, 'phone' => $company->phone];
+        })->all();
+        $this->assertEquals($expected, $filteredCompanies);
+    }
+
+}
